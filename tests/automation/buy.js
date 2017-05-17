ef61edabe25f80a75311fc7aeb0d3849fc1c03d9
sunasra <sunasra@gmail.com>
2017-05-17 10:37:23 +0530

    removed unwanted code

--- tests/automation/buy.js
+++ tests/automation/buy.js
@@ -1,15 +1,15 @@
 module.exports = {
     'If Add to Cart is avaialble' : function(browser){
         browser.url('http://localhost:3000')
-        .waitForElementVisible('body',3000)
-        .assert.containsText('#addToCart','ADD TO CART')
-        .end()
+        .waitForElementVisible('body',3000);
+        browser.expect.element('#addToCart').to.be.present;
+        browser.end()
     },
     'If Pick up in store is available' : function(browser){
         browser.url('http://localhost:3000')
-        .waitForElementVisible('body',3000)
-        .assert.containsText('#pickUpStore','PICK UP IN STORE')
-        .end()
+        .waitForElementVisible('body',3000);
+        browser.expect.element('#pickUpStore').to.be.present;
+        browser.end();
     },
     'If add to cart is not available' : function(browser){
         browser.url('http://localhost:3000')
